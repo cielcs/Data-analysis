491b7559fd1885efa852d07eaefed3e859f56216
cielcs <shotalicewww2@gmail.com>
2024-07-19 03:24:43 +0900

    remove build directory

--- test/build/_deps/eigen-build/bench/spbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shota/programs/dataanalysisc++/test/build/_deps/eigen-src/bench/spbench
-# Build directory: /Users/shota/programs/dataanalysisc++/test/build/_deps/eigen-build/bench/spbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test/build/_deps/eigen-build/unsupported/doc/snippets/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shota/programs/dataanalysisc++/test/build/_deps/eigen-src/unsupported/doc/snippets
-# Build directory: /Users/shota/programs/dataanalysisc++/test/build/_deps/eigen-build/unsupported/doc/snippets
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
