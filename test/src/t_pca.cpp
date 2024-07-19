e6e8ac7d89347b42942e4dbc84dc1c7c779dee2e
cielcs <shotalicewww2@gmail.com>
2024-07-19 16:40:48 +0900

    dont use namespace

--- test/src/t_pca.cpp
+++ test/src/t_pca.cpp
@@ -1,30 +1,28 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-using namespace Eigen;
-using namespace std;
 
 int main() {
-    MatrixXd data(3, 2);
+    Eigen::MatrixXd data(3, 2);
     data << 2.5, 2.4,
             0.5, 0.7,
             2.2, 2.9;
 
-    VectorXd mean = data.colwise().mean();
-    MatrixXd centered = data.rowwise() - mean.transpose();
+    Eigen::VectorXd mean = data.colwise().mean();
+    Eigen::MatrixXd centered = data.rowwise() - mean.transpose();
 
-    MatrixXd cov = (centered.adjoint() * centered) / double(data.rows() - 1);
+    Eigen::MatrixXd cov = (centered.adjoint() * centered) / double(data.rows() - 1);
 
-    SelfAdjointEigenSolver<MatrixXd> eig(cov);
-    MatrixXd eigenvectors = eig.eigenvectors();
-    VectorXd eigenvalues = eig.eigenvalues();
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(cov);
+    Eigen::MatrixXd eigenvectors = eig.eigenvectors();
+    Eigen::VectorXd eigenvalues = eig.eigenvalues();
 
-    cout << "Eigenvectors:" << endl << eigenvectors << endl;
-    cout << "Eigenvalues:" << endl << eigenvalues << endl;
+    std::cout << "Eigenvectors:" << std::endl << eigenvectors << std::endl;
+    std::cout << "Eigenvalues:" << std::endl << eigenvalues << std::endl;
 
-    MatrixXd transformed = centered * eigenvectors;
+    Eigen::MatrixXd transformed = centered * eigenvectors;
 
-    cout << "Transformed data:" << endl << transformed << endl;
+    std::cout << "Transformed data:" << std::endl << transformed << std::endl;
 
     return 0;
 }
